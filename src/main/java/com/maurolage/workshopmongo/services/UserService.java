d20c933bde2f63c96a44bac1810dd69ffd76a8e2
MauroLage <maurolage@hotmail.com>
2020-03-26 19:06:51 -0300

    Insercao de usuario com POST

--- src/main/java/com/maurolage/workshopmongo/services/UserService.java
+++ src/main/java/com/maurolage/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maurolage.workshopmongo.domain.User;
+import com.maurolage.workshopmongo.dto.UserDTO;
 import com.maurolage.workshopmongo.repository.UserRepository;
 import com.maurolage.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,15 +22,15 @@ public class UserService {
 	}
 	
 	public User findById(String id) {
-		// Optional<User> user = repo.findById(id);
-		//User user = repo.findOne(id);
-		
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		
-//		 if (user == null) {
-//			 throw new ObjectNotFoundException("Objeto não encontrado");
-//		 }
-//		 return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
